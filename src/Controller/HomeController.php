8a76bb0a8a1f07b905670712e96895c8deed739d
Jesper Kristensen <cableman@linuxdev.dk>
2020-05-23 17:52:01 +0200

    Basic app config

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig');
+    }
+    /**
+     * @Route("/personal-home-page", name="personal")
+     */
+    public function personal()
+    {
+        return $this->render('personal.html.twig');
+    }
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return;
+    }
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        return;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auth_token;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auth_token_expires;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return null;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getAuthToken(): ?string
+    {
+        return $this->auth_token;
+    }
+
+    public function setAuthToken(string $auth_token): self
+    {
+        $this->auth_token = $auth_token;
+
+        return $this;
+    }
+
+    public function getAuthTokenExpires(): ?string
+    {
+        return $this->auth_token_expires;
+    }
+
+    public function setAuthTokenExpires(string $auth_token_expires): self
+    {
+        $this->auth_token_expires = $auth_token_expires;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return [
+            'ROLE_USER'
+        ];
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->name,
+            $this->email,
+            $this->token
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list($this->id,
+            $this->name,
+            $this->email,
+            $this->token) = unserialize($serialized);
+    }
+}
