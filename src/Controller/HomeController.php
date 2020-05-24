3c16ba3b19d4257a6be2d0b34e0e2ba7e34efd3f
Jesper Kristensen <cableman@linuxdev.dk>
2020-05-24 15:37:26 +0200

    Got mail working

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 class HomeController extends AbstractController
 {
     /**
@@ -11,6 +15,7 @@ class HomeController extends AbstractController
     {
         return $this->render('home.html.twig');
     }
+
     /**
      * @Route("/personal-home-page", name="personal")
      */
@@ -18,13 +23,38 @@ class HomeController extends AbstractController
     {
         return $this->render('personal.html.twig');
     }
+
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", methods={"get"}, name="login")
      */
     public function login()
     {
-        return;
+        return $this->render('login.html.twig');
     }
+
+    /**
+     * @Route("/login", methods={"post"}, name="loginPost")
+     */
+    public function postlogin(Request $request, \Swift_Mailer $mailer)
+    {
+        $mail = $request->request->get('_mail');
+
+        $url = 'test';
+
+        $message = new \Swift_Message('Hello Email');
+        $message->setFrom('no-reply@local.itkdev.dk')
+            ->setTo($mail)
+            ->setBody(
+                $this->renderView('magic.html.twig', ['url' => $url]),
+                'text/html'
+            )
+        ;
+
+        $mailer->send($message);
+
+        return  $this->render('mail.html.twig');
+    }
+
     /**
      * @Route("/logout", name="logout")
      */
